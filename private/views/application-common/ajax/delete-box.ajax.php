4ae99510525cfd8e7744538cc584853c40136a34
Alesia <alesia@ketri.al>
2022-07-29 16:53:02 +0200

    Fixed a bug in the "delete-box.ajax.php" file. Added the correct queries for product validation and delete operation.

--- private/views/application-common/ajax/delete-box.ajax.php
+++ private/views/application-common/ajax/delete-box.ajax.php
@@ -3,7 +3,8 @@
 if ($_POST['action'] === 'deleteBox') {
   
   $_POST = array_map('trim', $_POST);
-  $errors = [];
+  
+#################################################################################################### --- INPUT VALIDATION  
   
   if (isEmpty(filter_input(INPUT_POST, 'prod_id', FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => SYSTEM_REGEX['any_digits_positive']]]))) {
     
@@ -16,63 +17,59 @@ if ($_POST['action'] === 'deleteBox') {
     exit;
   }
   
-  // verify if co_id exists on database
+  // Verify if product exists on database
   
   $verifyQuery = pg_query ($dbc['read_write'], sprintf ("
-    SELECT prod_id
-    FROM products
-    WHERE prod_id = '%s'
+    SELECT custom_prod_id
+    FROM custom_products
+    WHERE custom_prod_id = '%s'
+    AND custom_prod_owner_id = '%s'
     ",
-    pg_escape_string($dbc['read_write'], $_POST['prod_id'])
+    pg_escape_string($dbc['read_write'], $_POST['prod_id']),
+    pg_escape_string($dbc['read_write'], $_SESSION['user_id'])
   ));
   
   if (pg_num_rows ($verifyQuery) !== 1) {
     
-    $errors[] = _('Box was not found.');
-  }
-  
-  if (isEmpty ($errors)) {
-  
-#################################################################################################### --- DELETE COMPANY
-
-    $deleteQuery = pg_query($dbc['read_write'], sprintf("
-      DELETE FROM products
-      WHERE prod_id = '%s'
-      
-      ",
-      pg_escape_string($dbc['read_write'], $_POST['prod_id'])
-    )
-    );
-    
-#################################################################################################### --- HANDLE FEEDBACK
-
-    if (pg_affected_rows($deleteQuery) === 1) {
-      
-      echo json_encode ([
-        'success'          => true,
-        'feedbackSummary'  => [_('Box was deleted successfully.')],
-        'feedbackType'     => 'confirmation'
-      ]);
-      
-    } else {
-      
-      echo json_encode ([
-        'success'          => false,
-        'feedbackSummary'  => [_('Box could not be deleted.')],
-        'feedbackType'     => 'attention'
-      ]);
-      exit;
-    }
-  }
-  
-#################################################################################################### --- DISPLAY ERRORS
-  if ($errors) {
     echo json_encode ([
       'success'          => false,
-      'feedbackSummary'  => [_('This box could not be deleted.')],
+      'feedbackSummary'  => [_('Box was not found.')],
       'feedbackType'     => 'attention'
     ]);
+    
+    exit;
   }
+  
+#################################################################################################### --- DELETE PRODUCT
+
+  $deleteQuery = pg_query($dbc['read_write'], sprintf("
+    DELETE FROM custom_products
+    WHERE custom_prod_id = '%s'
+    ",
+    pg_escape_string($dbc['read_write'], $_POST['prod_id'])
+  ));
+  
+#################################################################################################### --- HANDLE FEEDBACK
+
+  if (pg_affected_rows($deleteQuery) === 1) {
+    
+    echo json_encode ([
+      'success'          => true,
+      'feedbackSummary'  => [_('Box was deleted successfully.')],
+      'feedbackType'     => 'confirmation'
+    ]);
+    
+  } else {
+    
+    echo json_encode ([
+      'success'          => false,
+      'feedbackSummary'  => [_('Box could not be deleted.')],
+      'feedbackType'     => 'attention'
+    ]);
+    
+    exit;
+  }
+  
 }
 
 ?>
